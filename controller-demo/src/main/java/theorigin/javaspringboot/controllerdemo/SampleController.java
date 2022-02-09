bf681c4165dacdaffad6cbd79e4e03e413c98e44
dev-yoonjung <dear__yoon@naver.com>
2022-02-09 18:10:46 +0900

    HTML Template: Thymeleaf

--- controller-demo/src/main/java/theorigin/javaspringboot/controllerdemo/SampleController.java
+++ controller-demo/src/main/java/theorigin/javaspringboot/controllerdemo/SampleController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,5 +25,19 @@ public class SampleController {
 
         model.addAttribute("profiles", profiles);
         return "view-jsp";
+    }
+
+    @GetMapping("/sample-thyme")
+    public ModelAndView sampleThyme() {
+        logger.info("in sample thyme");
+        ModelAndView modelAndView = new ModelAndView();
+        List<SamplePayload> profiles = new ArrayList<>();
+        profiles.add(new SamplePayload("Adam", 22, "Student"));
+        profiles.add(new SamplePayload("Bradely", 29, "Accountant"));
+        profiles.add(new SamplePayload("Charlie", 35, "Developer"));
+
+        modelAndView.addObject("profiles", profiles);
+        modelAndView.setViewName("view-thyme");
+        return modelAndView;
     }
 }
