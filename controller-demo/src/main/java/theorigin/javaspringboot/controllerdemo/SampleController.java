f9a0b33f8e63799506cdad910a589d20e16b39c9
dev-yoonjung <dear__yoon@naver.com>
2022-02-09 17:56:21 +0900

    HTML Template: JSP

--- controller-demo/src/main/java/theorigin/javaspringboot/controllerdemo/SampleController.java
+++ controller-demo/src/main/java/theorigin/javaspringboot/controllerdemo/SampleController.java
@@ -3,27 +3,26 @@ package theorigin.javaspringboot.controllerdemo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
+@RequestMapping("view")
 public class SampleController {
     private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(@RequestParam(name = "id", required = false, defaultValue = "") String id) {
-        logger.info("Path: Hello");
-        logger.info("Query Param id: " + id);
-        return "/hello.html";
-    }
+    @GetMapping("/sample-jsp")
+    public String sampleJsp(Model model) {
+        logger.info("in sample jsp");
+        List<SamplePayload> profiles = new ArrayList<>();
+        profiles.add(new SamplePayload("Adam", 22, "Student"));
+        profiles.add(new SamplePayload("Bradely", 29, "Accountant"));
+        profiles.add(new SamplePayload("Charlie", 35, "Developer"));
 
-    @GetMapping(value = "/hello/{id}")
-    public String helloPath(@PathVariable("id") String id) {
-        logger.info("Path Variable is: " + id);
-        return "/hello.html";
-    }
-
-    @GetMapping("/get-profile")
-    public @ResponseBody SamplePayload getProfile() {
-        return new SamplePayload("yoonjung", 27, "developer");
+        model.addAttribute("profiles", profiles);
+        return "view-jsp";
     }
 }
