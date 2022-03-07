92b9ee56dc8ddf87aa3496ae641a6ab3453955ac
dev-yoonjung <dear__yoon@naver.com>
2022-03-07 14:15:20 +0900

    6주차 Challenge mission

--- Mission/3차/Challenge/src/main/java/theorigin/javaspringboot/challenge/model/entity/CategoryEntity.java
+++ Mission/3차/Challenge/src/main/java/theorigin/javaspringboot/challenge/model/entity/CategoryEntity.java
@@ -1,8 +1,6 @@
 package theorigin.javaspringboot.challenge.model.entity;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "category")
--- Mission/3차/Challenge/src/main/java/theorigin/javaspringboot/challenge/model/entity/RoleEntity.java
+++ Mission/3차/Challenge/src/main/java/theorigin/javaspringboot/challenge/model/entity/RoleEntity.java
@@ -2,9 +2,7 @@ package theorigin.javaspringboot.challenge.model.entity;
 
 import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Table(name = "role")
