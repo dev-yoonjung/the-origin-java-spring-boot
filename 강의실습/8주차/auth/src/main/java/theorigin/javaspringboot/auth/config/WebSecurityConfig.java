5c8d3a728b90770457e792654c527450a2b7697e
dev-yoonjung <dear__yoon@naver.com>
2022-03-17 20:25:33 +0900

    OAuth2와 JWT

--- 강의실습/8주차/auth/src/main/java/theorigin/javaspringboot/auth/config/WebSecurityConfig.java
+++ 강의실습/8주차/auth/src/main/java/theorigin/javaspringboot/auth/config/WebSecurityConfig.java
@@ -1,24 +1,25 @@
 package theorigin.javaspringboot.auth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import theorigin.javaspringboot.auth.infra.CustomUserDetailsService;
+import theorigin.javaspringboot.auth.infra.NaverOAuth2Service;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
+    private final NaverOAuth2Service naverOAuth2Service;
 
-    public WebSecurityConfig(@Autowired CustomUserDetailsService customUserDetailsService) {
+    public WebSecurityConfig(@Autowired CustomUserDetailsService customUserDetailsService,
+                             @Autowired NaverOAuth2Service naverOAuth2Service) {
+        this.naverOAuth2Service = naverOAuth2Service;
         this.userDetailsService = customUserDetailsService;
     }
 
@@ -38,22 +39,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/home/**", "/user/signup/**")
-                .anonymous()
-                .anyRequest()
-                .authenticated()
+                    .authorizeRequests()
+                    .antMatchers("/home/**", "/user/signup/**")
+                    .anonymous()
+                    .anyRequest()
+                    .authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/user/login")
-                .defaultSuccessUrl("/home")
-                .permitAll()
+                    .formLogin()
+                    .loginPage("/user/login")
+                    .defaultSuccessUrl("/home")
+                    .permitAll()
                 .and()
-                .logout()
-                .logoutUrl("/user/logout")
-                .logoutSuccessUrl("/home")
-                .deleteCookies("JSESSIONID")
-                .invalidateHttpSession(true)
-                .permitAll();
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                        .userService(this.naverOAuth2Service)
+                    .and()
+                        .defaultSuccessUrl("/home")
+                .and()
+                    .logout()
+                    .logoutUrl("/user/logout")
+                    .logoutSuccessUrl("/home")
+                    .deleteCookies("JSESSIONID")
+                    .invalidateHttpSession(true)
+                    .permitAll();
     }
 }
--- 강의실습/8주차/auth/src/main/java/theorigin/javaspringboot/auth/controller/HomeController.java
+++ 강의실습/8주차/auth/src/main/java/theorigin/javaspringboot/auth/controller/HomeController.java
@@ -3,7 +3,6 @@ package theorigin.javaspringboot.auth.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
