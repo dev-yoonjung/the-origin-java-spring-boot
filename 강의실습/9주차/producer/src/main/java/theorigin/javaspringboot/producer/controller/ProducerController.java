280d9659da98c5811354bbb38d07798cb378d706
dev-yoonjung <dear__yoon@naver.com>
2022-03-23 04:44:52 +0900

    Messsage Broker 활용

---
+++ 강의실습/9주차/producer/src/main/java/theorigin/javaspringboot/producer/controller/ProducerController.java
@@ -0,0 +1,22 @@
+package theorigin.javaspringboot.producer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import theorigin.javaspringboot.producer.service.ProducerService;
+
+@RestController
+public class ProducerController {
+
+    private final ProducerService producerService;
+
+    public ProducerController(@Autowired ProducerService producerService) {
+        this.producerService = producerService;
+    }
+
+    @GetMapping("/")
+    public void sendMessage() {
+        producerService.send();
+    }
+
+}
---
+++ 강의실습/9주차/publisher/src/main/java/theorigin/javaspringboot/publisher/controller/PublisherController.java
@@ -0,0 +1,20 @@
+package theorigin.javaspringboot.publisher.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import theorigin.javaspringboot.publisher.service.PublisherService;
+
+@RestController
+public class PublisherController {
+    private final PublisherService publisherService;
+
+    public PublisherController(@Autowired PublisherService publisherService) {
+        this.publisherService = publisherService;
+    }
+
+    @GetMapping("/")
+    public void sendMessage() {
+        publisherService.publishMessage();
+    }
+}
